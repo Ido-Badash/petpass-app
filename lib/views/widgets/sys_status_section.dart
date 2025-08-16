80f4cca85c8bf16e69aa995a0fb7bed308438b22
Ido-Badash <idoba12012011@gmail.com>
2025-08-16 16:32:22 +0300

    ✨ Fix import order in SysStatusSection: rearrange import statements for consistency.

--- lib/views/widgets/sys_status_section.dart
+++ lib/views/widgets/sys_status_section.dart
@@ -1,7 +1,7 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/material.dart";
-import "package:petpass/views/widgets/default_circular_progress_indicator.dart";
 import "package:petpass/utils/db_helpers.dart";
+import "package:petpass/views/widgets/default_circular_progress_indicator.dart";
 
 class SysStatusSection extends StatelessWidget {
   const SysStatusSection({super.key});
